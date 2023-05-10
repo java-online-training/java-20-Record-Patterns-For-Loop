ebc7e43c9b32304d55458eb2e0431fd1a10d3d43
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-05-10 12:07:54 +0200

    example for enhanced for loop

--- src/main/java/RecordPatternsForLoopExample.java
+++ src/main/java/RecordPatternsForLoopExample.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class RecordPatternsForLoppExample {
+import java.util.List;
+
+public class RecordPatternsForLoopExample {
+
+
+    record Monkey(String name, int age){}
+    
+    public static void main(String[] args) {
+        List monkes = List.of(new Monkey("Bert", 2), new Monkey("Jeff", 5), new Monkey("Ernie", 7));
+        
+        for (Monkey m : monkeys){
+            System.out.println(m.name);
+        }
+    }
 }
